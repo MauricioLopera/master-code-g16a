1b8341d4f24f499cad83d22a6600e72a490476a8
Mauricio Lopera <sistemas@helpex.co>
2022-05-12 20:02:23 -0500

    Bootstrap

---
+++ 02-Intro_a_JS/13-Bootstrap/js/app.js
@@ -0,0 +1,5 @@
+function Reordenar(){
+    document.getElementById('1').classList.add('order-3');
+    document.getElementById('2').classList.add('order-1');
+    document.getElementById('3').classList.add('order-2');
+}
